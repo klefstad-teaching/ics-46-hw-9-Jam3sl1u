a46ea7b1f2aa6ea6e3aa3fcff643a3f0ad1e59c7
James Liu <jliuzhishun@gmail.com>
2025-03-14 06:36:12 +0000

    extract_shortest_path Function Built
    function takes the distances from the distance vector and pushes into a new vector moving through the previous using a destination index, reverses the vector and then returns as output

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -36,5 +36,16 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distance;
 }
 
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
-void print_path(const vector<int>& v, int total);
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> shortest_path;
+    for (int idx = destination; idx != -1; idx = previous[idx]) {
+        shortest_path.push_back(idx);
+    }
+    if (shortest_path.empty() != false) {
+        reverse(shortest_path.begin(), shortest_path.end());
+    }
+    return shortest_path;
+}
+
+void print_path(const vector<int>& v, int total) {
+

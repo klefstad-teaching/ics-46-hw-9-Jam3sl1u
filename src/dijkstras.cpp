e0d44e23e07430bb2bbedeb44dc8e1a43133eaba
James Liu <jliuzhishun@gmail.com>
2025-03-14 06:17:36 +0000

    dijsktras_shortest path algorithm Function Built
    funciton uses dijsktras algorithm with a priority queue and vectors in order to visit verticies until the the path is determined

---
+++ src/dijkstras.cpp
@@ -0,0 +1,40 @@
+#include "dijkstras.h"
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+
+    vector<int> distance(n, INF);
+    distance[source] = 0;
+    vector<bool> visited(n, false);
+
+    previous.resize(n, -1);
+
+    priority_queue<pair<int, int>> pq;
+    pq.push({source, 0});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        if (visited[u] == true) {
+            continue;
+        }
+        visited[u] = true;
+        for (Edge neighbor : G[u]) {
+            int v = neighbor.dst;
+            int weight = neighbor.weight;
+            if (!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({v, distance[v]});
+            }
+        }
+    }
+    return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
+void print_path(const vector<int>& v, int total);

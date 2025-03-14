7069009500ad4a1a769012cb4ba570227c7bd001
James Liu <jliuzhishun@gmail.com>
2025-03-14 21:06:40 +0000

    verify_word_ladder() Function Built
    function uses assertion statements to test if the word ladder performs correctly

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 #include <vector>
 #include <set>
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
 using namespace std;
 
@@ -63,4 +64,13 @@ void print_word_ladder(const vector<string>& ladder) {
     }
 }
 
-void verify_word_ladder();
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "src/words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
